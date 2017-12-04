8f680f6e6ac68a4bca2abfd27aa2cdcaa99b04c2
Temur Mirzosharipov <timurterminti@gmail.com>
2017-12-04 23:00:49 +1300

    Update restricted_sum.py

--- restricted_sum.py
+++ restricted_sum.py
@@ -1,15 +1,8 @@
-def checkio(numbers):
-	ans , t,q = 0,0, len(numbers)
-	if q!=0:
-		q,ans,t = test(q,ans,t,numbers)
-	print q,ans,t
-	return ans
-	
-def test(q,ans,t,numbers):
-	ans +=numbers[0+t]
-	t +=1
-	q -=1
+def checkio(data):
+    if len(data) == 1:
+        return data[0]
+    else:
+        return data[0] + checkio(data[1:])
 
-	return q,ans,t	
-	
-print checkio([1,2])
+
+checkio([1, 2, 3])
